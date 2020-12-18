77866d670adee7f897e22868b87c0e8ea6e9edfc
Karan <gurkaran1314@gmail.com>
2020-12-19 03:44:33 +0530

    Minor bug fix

--- app.js
+++ app.js
@@ -196,6 +196,6 @@ app.get("/players", function(req, res) {
 })
 
 
-pp.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
